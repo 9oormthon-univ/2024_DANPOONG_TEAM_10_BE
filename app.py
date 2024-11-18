f1d0b6db4276318a06b77a01b6f1131d9f1866dd
SongMinhyuk <dnfldpden32@gmail.com>
2024-11-18 23:57:28 +0900

    FEAT: POSTGRE 로컬 DB 연결

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+#id:admin pw:admin
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/passionpay_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return "DB연결 성공"
 
 if __name__ == '__main__':
-    app.run()
-    
+    app.run(debug=True)
