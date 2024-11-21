44d60593120f12eef82c1bd94599fe9a413e59e8
SongMinhyuk <dnfldpden32@gmail.com>
2024-11-21 22:32:05 +0900

    FEAT: 사용자,기본 모델 생성

---
+++ dto/member_model.py
@@ -0,0 +1,16 @@
+from .base_model import BaseTable
+from ..db_config import db
+
+
+# db = SQLAlchemy()
+
+class Member(BaseTable):
+    #TODO: UUID로 변경
+    id = db.Column(db.Integer, primary_key=True,nullable=False)
+    nickname = db.Column(db.String(10), nullable=True)
+    birth_date = db.Column(db.Date, nullable=True)
+    kakao_id = db.Column(db.Integer,unique=True)
+    gender=db.Column(db.String(10))
+
+    def __init__(self, kakao_id):
+        self.kakao_id=kakao_id
