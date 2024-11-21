7e363922622a32736a2424bdf35129b698e9e7bd
SongMinhyuk <dnfldpden32@gmail.com>
2024-11-21 22:33:18 +0900

    FEAT: 기본 APP 로직 변경

--- app.py
+++ app.py
@@ -1,20 +1,49 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api
+from dotenv import load_dotenv
+import os
+from .routes.signup import signup
+from .oauth import kakao
+from .db_config import db
+
+load_dotenv()  # .env 파일 로드
 
 app = Flask(__name__)
+port = os.getenv('FLASK_RUN_PORT', 5001)  # 기본값 5000
 
-#id:admin pw:admin
+# #id:admin pw:admin
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/passionpay_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
+db.init_app(app)
 with app.app_context():
     db.create_all()
+
+# Swagger UI 설정
+authorizations = {
+    'Bearer': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Authorization'
+    }
+}
+
+api = Api(
+    app,
+    version='1.0',
+    title='회원가입 API',
+    description='회원가입 관련 API 문서',
+    authorizations=authorizations,
+    security='Bearer'
+)
+
+api.add_namespace(kakao, path='/oauth/kakao')
+api.add_namespace(signup, path='/signup')
+
 
 @app.route('/')
 def index():
     return "DB연결 성공"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True ,port=port)
