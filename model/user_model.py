e0a2912b3bfe25882de5bdf2b60b6186b866bdfe
SongMinhyuk <dnfldpden32@gmail.com>
2024-11-22 17:18:06 +0900

    MOVE: 기존 dto 폴더 Model 폴더로 변경

--- model/user_model.py
+++ model/user_model.py
@@ -4,7 +4,7 @@ from ..db_config import db
 
 # db = SQLAlchemy()
 
-class Member(BaseTable):
+class User(BaseTable):
     #TODO: UUID로 변경
     id = db.Column(db.Integer, primary_key=True,nullable=False)
     nickname = db.Column(db.String(10), nullable=True)
@@ -14,3 +14,5 @@ class Member(BaseTable):
 
     def __init__(self, kakao_id):
         self.kakao_id=kakao_id
+    def __repr__(self):
+        return f'<User {self.kakao_id}>'
