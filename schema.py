09874ea880559beb96a5178bb2d69b0538b7d7e7
CHANHUN JEON <cksgns080101@gmail.com>
2024-11-24 06:17:10 +0900

    add festival

--- schema.py
+++ schema.py
@@ -5,13 +5,14 @@ from queries.review_queries import ReviewQueries
 from queries.user_agree_queries import UserAgreeQueries
 from queries.user_festival_like_queries import UserFestivalLikeQueries
 from queries.stamp_queries import StampQueries
+from queries.festival_queries import FestivalQueries
 from mutations.user_mutations import UserMutations
 from mutations.user_agree_mutations import UserAgreeMutations
 from mutations.review_mutations import ReviewMutations
 from mutations.user_festival_like_mutations import UserFestivalLikeMutations
 
 
-class Query(UserQueries, TermsQueries, ReviewQueries,UserAgreeQueries,UserFestivalLikeQueries,StampQueries,graphene.ObjectType):
+class Query(FestivalQueries, UserQueries, TermsQueries, ReviewQueries,UserAgreeQueries,UserFestivalLikeQueries,StampQueries,graphene.ObjectType):
     pass
 
 class Mutation(UserMutations, ReviewMutations,UserAgreeMutations, UserFestivalLikeMutations,graphene.ObjectType):
